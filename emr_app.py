2a5f0fc68acbeacbed974bdbd055c41f0c0cc8b7
Marcin Knara <marcinknara@gmail.com>
2024-11-25 16:41:31 -0500

    22

--- emr_app.py
+++ emr_app.py
@@ -79,7 +79,7 @@ setup_logging()
 class EMRManager(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Case Manager v1.0.21")
+        self.setWindowTitle("Case Manager v1.0.22")
         self.setGeometry(100, 100, 900, 600)
         
         # Main layout
